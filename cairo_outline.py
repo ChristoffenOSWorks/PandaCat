408c6a5e5112a5d0aa1c0f503ebf45ac1acd0908
M. Gage Morgan <gage@christoffen.com>
2017-02-27 16:00:31 -0500

    Create cairo_outline.py

---
+++ cairo_outline.py
@@ -0,0 +1,7 @@
+startx = int(raw_input("Enter the starting X value"))
+starty = int(raw_input("Enter the starting Y value"))
+
+print "cairo_move_to(cr, " + str(startx) + ", " + str(starty) + ");"
+
+
+
